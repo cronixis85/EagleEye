0b3881f6712cb2d5468b39b12c229bbe43aa106c
Wei Ru Tay <weiru85@hotmail.com>
2017-12-09 22:41:30 +0800

    can deploy webjob

--- EagleEye.Extractor.Job/Functions.cs
+++ EagleEye.Extractor.Job/Functions.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.IO;
 using Microsoft.Azure.WebJobs;
 
-namespace EagleEye.Extractor.WebJob
+namespace EagleEye.Extractor.Job
 {
     public class Functions
     {
---
+++ EagleEye.Extractor.Job/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EagleEye.Extractor.Job
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var config = new JobHostConfiguration();
+
+            if (config.IsDevelopment)
+                config.UseDevelopmentSettings();
+
+            var host = new JobHost(config);
+            host.RunAndBlock();
+        }
+        private static void ConfigureServices(IServiceCollection serviceCollection)
+        {
+            // Setup your container here, just like a asp.net core app
+
+            // Optional: Setup your configuration:
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+
+            //serviceCollection.Configure<MySettings>(configuration);
+
+            //// A silly example of wiring up some class used by the web job:
+            //serviceCollection.AddScoped<ISomeInterface, SomeUsefulClass>();
+            //// Your classes that contain the webjob methods need to be DI-ed up too
+            //serviceCollection.AddScoped<WebJobsMethods, WebJobsMethods>();
+
+            // One more thing - tell azure where your azure connection strings are
+            Environment.SetEnvironmentVariable("AzureWebJobsDashboard", configuration.GetConnectionString("WebJobsDashboard"));
+            Environment.SetEnvironmentVariable("AzureWebJobsStorage", configuration.GetConnectionString("WebJobsStorage"));
+        }
+    }
+}
